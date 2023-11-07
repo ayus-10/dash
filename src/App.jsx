f5b3993fec2770e17488e69f2a3d6313c09a9d12
Aayush <136055230+ayus-10@users.noreply.github.com>
2023-11-07 11:35:27 +0545

    minor style changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Sidebar from './components/Sidebar.jsx'
 const App = () => {
     return (
         <div className='grid grid-cols-5 w-screen'>
-            <Sidebar className='col-span-2' />
+            <Sidebar />
         </div>
     )
 }
