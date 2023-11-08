ee512220eedc98c4b2e52499b3afe48b9748e210
Aayush <136055230+ayus-10@users.noreply.github.com>
2023-11-08 10:57:45 +0545

    made the sidebar functional, plus some design changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './components/Sidebar.jsx'
 import Home from './components/Home.jsx'
 import About from './components/About.jsx'
@@ -7,9 +7,21 @@ import Pricing from './components/Pricing.jsx'
 import Support from './components/Support.jsx'
 
 const App = () => {
+    const [activeComponent, setActiveComponent] = useState('home')
+    const handleButtonClick = (componentName) => {
+        setActiveComponent(componentName)
+    }
+
     return (
         <div className='grid grid-cols-6 w-screen'>
-            <Sidebar />
+            <Sidebar onButtonClick={handleButtonClick} activeComponent={activeComponent} />
+            <main className="col-span-5">
+                {activeComponent === 'home' && <Home />}
+                {activeComponent === 'about' && <About />}
+                {activeComponent === 'services' && <Services />}
+                {activeComponent === 'pricing' && <Pricing />}
+                {activeComponent === 'support' && <Support />}
+            </main>
         </div>
     )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,38 +4,51 @@ import { MdInfo } from 'react-icons/md'
 import { BiDollarCircle, BiSupport, BiSolidHome } from 'react-icons/bi'
 import Logo from '../assets/react.svg'
 
-const Sidebar = () => {
+const Sidebar = (props) => {
+    var active = props.activeComponent
+
     return (
-        <div className='col-span-1'>
-            <div className='bg-slate-800 h-screen flex flex-col'>
+        <header className='col-span-1'>
+            <nav className='bg-slate-900 h-screen flex flex-col'>
                 <div className='text-white text-xl'>
-                    <a className='flex items-center justify-center m-4 gap-2' href='#'>
+                    <div className='flex items-center justify-center m-4 cursor-pointer gap-1'
+                        onClick={() => props.onButtonClick('home')}>
                         <img src={Logo} alt='logo' />
-                        <h1 className='lg:text-4xl text-3xl'>DASH</h1>
-                    </a>
-                    <a className='bg-slate-600 hover:bg-slate-700 ease-in-out duration-300 p-2 m-2 rounded-md flex items-center justify-center gap-1' href='#'>
+                        <h1 className='lg:text-4xl text-3xl md:block hidden'>DASH</h1>
+                    </div>
+                    <div
+                        className={`hover:bg-slate-800 ease-in-out duration-300 p-2 m-2 cursor-pointer rounded-md flex items-center justify-center gap-1 ${active === 'home' ? 'bg-slate-800' : 'bg-slate-700'}`}
+                        onClick={() => props.onButtonClick('home')}>
                         <BiSolidHome className='inline flex-shrink-0' />
                         <span className='md:inline hidden'>Home</span>
-                    </a>
-                    <a className='bg-slate-600 hover:bg-slate-700 ease-in-out duration-300 p-2 m-2 rounded-md flex items-center justify-center gap-1' href='#'>
+                    </div>
+                    <div
+                        className={`hover:bg-slate-800 ease-in-out duration-300 p-2 m-2 cursor-pointer rounded-md flex items-center justify-center gap-1 ${active === 'about' ? 'bg-slate-800' : 'bg-slate-700'}`}
+                        onClick={() => props.onButtonClick('about')}>
                         <MdInfo className='inline flex-shrink-0' />
                         <span className='md:inline hidden'>About</span>
-                    </a>
-                    <a className='bg-slate-600 hover:bg-slate-700 ease-in-out duration-300 p-2 m-2 rounded-md flex items-center justify-center gap-1' href='#'>
+                    </div>
+                    <div
+                        className={`hover:bg-slate-800 ease-in-out duration-300 p-2 m-2 cursor-pointer rounded-md flex items-center justify-center gap-1 ${active === 'services' ? 'bg-slate-800' : 'bg-slate-700'}`}
+                        onClick={() => props.onButtonClick('services')}>
                         <PiGearSixBold className='inline flex-shrink-0' />
                         <span className='md:inline hidden'>Services</span>
-                    </a>
-                    <a className='bg-slate-600 hover:bg-slate-700 ease-in-out duration-300 p-2 m-2 rounded-md flex items-center justify-center gap-1' href='#'>
+                    </div>
+                    <div
+                        className={`hover:bg-slate-800 ease-in-out duration-300 p-2 m-2 cursor-pointer rounded-md flex items-center justify-center gap-1 ${active === 'pricing' ? 'bg-slate-800' : 'bg-slate-700'}`}
+                        onClick={() => props.onButtonClick('pricing')}>
                         <BiDollarCircle className='inline flex-shrink-0' />
                         <span className='md:inline hidden'>Pricing</span>
-                    </a>
-                    <a className='bg-slate-600 hover:bg-slate-700 ease-in-out duration-300 p-2 m-2 rounded-md flex items-center justify-center gap-1' href='#'>
+                    </div>
+                    <div
+                        className={`hover:bg-slate-800 ease-in-out duration-300 p-2 m-2 cursor-pointer rounded-md flex items-center justify-center gap-1 ${active === 'support' ? 'bg-slate-800' : 'bg-slate-700'}`}
+                        onClick={() => props.onButtonClick('support')}>
                         <BiSupport className='inline flex-shrink-0' />
                         <span className='md:inline hidden'>Support</span>
-                    </a>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </nav>
+        </header >
     )
 }
 
