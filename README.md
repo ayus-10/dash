f6fabbf2231fc4fe0f9bf40667ebb240eb449d57
Aayush <136055230+ayus-10@users.noreply.github.com>
2023-11-10 20:23:53 +0545

    improved styling for contact component, added a readme file

---
+++ README.md
@@ -0,0 +1,16 @@
+# About
+
+This is a single page application built using React(Vite) and Tailwind CSS.
+
+# Installation
+
+1. First make sure that you have **npm** and **Node JS** installed.
+2. Clone this repo and open the folder in a terminal.
+3. Run these commands
+
+   ```
+    npm install
+    npm run build
+   ```
+
+4. The app will be build in the **dist** folder, run it using live server.
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,51 +1,68 @@
 import React from "react";
+import { HiOutlineLocationMarker } from "react-icons/hi";
 
 const Contact = () => {
   const locationOnMap =
-    "https://www.openstreetmap.org/export/embed.html?bbox=12.861591875553131%2C48.06702809880262%2C12.86364644765854%2C48.06799061669401&amp;layer=mapnik";
+    "https://www.openstreetmap.org/export/embed.html?bbox=-73.98702174425127%2C40.74789704963033%2C-73.98496717214586%2C40.74898823106307&amp;layer=mapnik&amp;marker=40.74844264258488%2C-73.98599445819855";
+  const location = "350 Empire State Building, 5th Avenue, New York";
 
   return (
-    <div className="md:p-12 p-4 text-gray-900 h-full overflow-x-hidden">
-      <h1 className="text-sky-700 md:text-4xl text-2xl font-medium uppercase mb-2">
+    <div className="md:p-12 p-4 text-gray-900">
+      <h1 className="text-sky-700 md:text-4xl text-2xl font-medium uppercase mb-4">
         Contact
       </h1>
-      <div className="relative md:h-full md:w-full flex flex-col">
-        <iframe src={locationOnMap} className="md:h-full md:w-full"></iframe>
-        <div className="md:absolute md:top-1/2 md:transform md:-translate-y-1/2 md:right-0 md:mx-4 p-4 flex flex-col gap-4 bg-slate-100 h-fit w-fit rounded-md md:text-lg">
-          <h1 className="md:text-lg text-sky-700 font-medium uppercase">
-            Send us a message!
+      <div className="relative md:h-screen w-full flex flex-col">
+        <div className="md:hidden flex flex-col mb-2">
+          <h1 className="text-sky-700 font-medium uppercase">Our address</h1>
+          <p className="text-sm">{location}</p>
+        </div>
+        <iframe src={locationOnMap} height="100%" width="100%"></iframe>
+        <div className="md:absolute md:top-1/2 md:transform md:-translate-y-1/2 md:right-8 my-4 md:p-4 bg-slate-100 rounded-md">
+          <div className="md:flex md:flex-col mb-4 hidden">
+            <h1 className="text-lg text-sky-700 font-medium uppercase">
+              Our address
+            </h1>
+            <p className="flex gap-1 items-center text-base">
+              <HiOutlineLocationMarker className="flex-shrink-0" />
+              {location}
+            </p>
+          </div>
+          <h1 className="md:text-lg text-sky-700 font-medium uppercase mb-2">
+            Send us a message
           </h1>
-          <div className="flex flex-col text-gray-700 md:text-sm text-xs">
-            <label htmlFor="userName">Your name</label>
-            <input
-              id="userName"
-              type="text"
-              className="p-2 outline-none rounded-sm bg-transparent border border-slate-400 focus:border-sky-700 focus:bg-white focus:ring-1 ease-in-out duration-500"
-              placeholder="John Cena"
-            />
+          <div className="flex flex-col gap-4 mb-4">
+            <div className="flex flex-col text-gray-700 md:text-sm text-xs">
+              <label htmlFor="userName">Your name</label>
+              <input
+                id="userName"
+                type="text"
+                className="p-2 outline-none rounded-sm bg-transparent border border-slate-400 focus:border-sky-700 focus:bg-white focus:ring-1 ease-in-out duration-500"
+                placeholder="John Cena"
+              />
+            </div>
+            <div className="flex flex-col text-gray-700 md:text-sm text-xs">
+              <label htmlFor="email">Email</label>
+              <input
+                id="email"
+                type="email"
+                className="p-2 outline-none rounded-sm bg-transparent border border-slate-400 focus:border-sky-700 focus:bg-white focus:ring-1 ease-in-out duration-500"
+                placeholder="johncena@example.com"
+              />
+            </div>
+            <div className="flex flex-col text-gray-700 md:text-sm text-xs">
+              <label htmlFor="message">Message</label>
+              <textarea
+                id="message"
+                cols="30"
+                rows="4"
+                className="p-2 outline-none rounded-sm bg-transparent border border-slate-400 focus:border-sky-700 focus:bg-white focus:ring-1 ease-in-out duration-500 resize-none"
+                placeholder="You can't see me, my time is now."
+              ></textarea>
+            </div>
+            <button className="bg-sky-700 hover:bg-sky-800 ease-in-out duration-300 text-white py-2 rounded-sm">
+              Send
+            </button>
           </div>
-          <div className="flex flex-col text-gray-700 md:text-sm text-xs">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              type="email"
-              className="p-2 outline-none rounded-sm bg-transparent border border-slate-400 focus:border-sky-700 focus:bg-white focus:ring-1 ease-in-out duration-500"
-              placeholder="johncena@example.com"
-            />
-          </div>
-          <div className="flex flex-col text-gray-700 md:text-sm text-xs">
-            <label htmlFor="message">Message</label>
-            <textarea
-              id="message"
-              cols="30"
-              rows="4"
-              className="p-2 outline-none rounded-sm bg-transparent border border-slate-400 focus:border-sky-700 focus:bg-white focus:ring-1 ease-in-out duration-500 resize-none"
-              placeholder="You can't see me, my time is now."
-            ></textarea>
-          </div>
-          <button className="bg-sky-700 hover:bg-sky-800 ease-in-out duration-300 text-white py-2 rounded-sm">
-            Send
-          </button>
         </div>
       </div>
     </div>
