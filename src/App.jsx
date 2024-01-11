e89e9b1af095e75ea1c6dfedd280d34e44b56586
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-01-11 12:22:45 +0545

    implemented react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar.jsx";
 import Home from "./components/Home.jsx";
 import About from "./components/About.jsx";
@@ -8,24 +9,30 @@ import Contact from "./components/Contact.jsx";
 import Footer from "./components/Footer.jsx";
 
 const App = () => {
-  const [activeComponent, setActiveComponent] = useState("home");
-  const handleButtonClick = (componentName) => {
-    setActiveComponent(componentName);
-  };
+  const [activeComponent, setActiveComponent] = useState("");
+
+  useEffect(() => {
+    const currentPath = window.location.pathname.replace("/", "");
+    if (currentPath !== activeComponent) {
+      setActiveComponent(currentPath);
+    }
+  }, []);
 
   return (
     <div className="grid h-screen w-screen grid-cols-6 overflow-hidden">
       <Sidebar
-        onButtonClick={handleButtonClick}
         activeComponent={activeComponent}
+        setActiveComponent={setActiveComponent}
       />
       <div className="col-span-5 flex h-full flex-col justify-between overflow-y-auto bg-slate-100">
         <main className="grow">
-          {activeComponent === "home" && <Home />}
-          {activeComponent === "about" && <About />}
-          {activeComponent === "services" && <Services />}
-          {activeComponent === "pricing" && <Pricing />}
-          {activeComponent === "contact" && <Contact />}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
         </main>
         <Footer />
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,67 +1,63 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { PiGearSixBold } from "react-icons/pi";
 import { MdInfo } from "react-icons/md";
 import { BiDollarCircle, BiSupport, BiSolidHome } from "react-icons/bi";
 import Logo from "../assets/logo.png";
 
 const Sidebar = (props) => {
-  var active = props.activeComponent;
+  const { activeComponent, setActiveComponent } = props;
+
+  const links = [
+    {
+      icon: <BiSolidHome className="inline flex-shrink-0" />,
+      name: "Home",
+      link: "",
+    },
+    {
+      icon: <MdInfo className="inline flex-shrink-0" />,
+      name: "About",
+      link: "about",
+    },
+    {
+      icon: <PiGearSixBold className="inline flex-shrink-0" />,
+      name: "Services",
+      link: "services",
+    },
+    {
+      icon: <BiDollarCircle className="inline flex-shrink-0" />,
+      name: "Pricing",
+      link: "pricing",
+    },
+    {
+      icon: <BiSupport className="inline flex-shrink-0" />,
+      name: "Contact",
+      link: "contact",
+    },
+  ];
 
   return (
     <header className="col-span-1 flex h-full flex-col bg-slate-900">
       <div className="text-xl text-white">
-        <div
-          className="m-2 flex cursor-pointer items-center justify-center gap-1"
-          onClick={() => props.onButtonClick("home")}
-        >
+        <div className="m-2 flex cursor-pointer items-center justify-center gap-1">
           <img src={Logo} alt="logo" className="max-h-10" />
           <h1 className="hidden text-3xl md:block lg:text-4xl">DASH</h1>
         </div>
-        <div
-          className={`m-2 flex cursor-pointer items-center justify-center gap-1 rounded-md p-2 duration-300 ease-in-out hover:bg-slate-800 ${
-            active === "home" ? "bg-slate-800" : "bg-slate-700"
-          }`}
-          onClick={() => props.onButtonClick("home")}
-        >
-          <BiSolidHome className="inline flex-shrink-0" />
-          <span className="hidden md:inline">Home</span>
-        </div>
-        <div
-          className={`m-2 flex cursor-pointer items-center justify-center gap-1 rounded-md p-2 duration-300 ease-in-out hover:bg-slate-800 ${
-            active === "about" ? "bg-slate-800" : "bg-slate-700"
-          }`}
-          onClick={() => props.onButtonClick("about")}
-        >
-          <MdInfo className="inline flex-shrink-0" />
-          <span className="hidden md:inline">About</span>
-        </div>
-        <div
-          className={`m-2 flex cursor-pointer items-center justify-center gap-1 rounded-md p-2 duration-300 ease-in-out hover:bg-slate-800 ${
-            active === "services" ? "bg-slate-800" : "bg-slate-700"
-          }`}
-          onClick={() => props.onButtonClick("services")}
-        >
-          <PiGearSixBold className="inline flex-shrink-0" />
-          <span className="hidden md:inline">Services</span>
-        </div>
-        <div
-          className={`m-2 flex cursor-pointer items-center justify-center gap-1 rounded-md p-2 duration-300 ease-in-out hover:bg-slate-800 ${
-            active === "pricing" ? "bg-slate-800" : "bg-slate-700"
-          }`}
-          onClick={() => props.onButtonClick("pricing")}
-        >
-          <BiDollarCircle className="inline flex-shrink-0" />
-          <span className="hidden md:inline">Pricing</span>
-        </div>
-        <div
-          className={`m-2 flex cursor-pointer items-center justify-center gap-1 rounded-md p-2 duration-300 ease-in-out hover:bg-slate-800 ${
-            active === "contact" ? "bg-slate-800" : "bg-slate-700"
-          }`}
-          onClick={() => props.onButtonClick("contact")}
-        >
-          <BiSupport className="inline flex-shrink-0" />
-          <span className="hidden md:inline">Contact</span>
-        </div>
+        {links.map((item, index) => {
+          return (
+            <Link
+              key={index}
+              to={`/${item.link}`}
+              onClick={() => setActiveComponent(item.link)}
+              className={`m-2 flex cursor-pointer items-center justify-center gap-1 rounded-md p-2 duration-300 ease-in-out hover:bg-slate-800 
+              ${activeComponent === item.link ? "bg-slate-800" : "bg-slate-700"}
+              `}
+            >
+              {item.icon}
+              <span className="hidden md:inline">{item.name}</span>
+            </Link>
+          );
+        })}
       </div>
     </header>
   );
